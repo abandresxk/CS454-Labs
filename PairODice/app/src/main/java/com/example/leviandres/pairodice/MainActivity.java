5d2760bc33c605ecad6f8292324ce128b5597103
abandres <abandres@xumak.com>
2015-04-20 21:48:01 -0700

    THIS IS PIG VERSION

--- PairODice/app/src/main/java/com/example/leviandres/pairodice/MainActivity.java
+++ PairODice/app/src/main/java/com/example/leviandres/pairodice/MainActivity.java
@@ -1,99 +1,177 @@
 package com.example.leviandres.pairodice;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.FrameStats;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.leviandres.pairodice.R;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
-    private FrameLayout die1,die2;
+    private FrameLayout die1, die2;
     private Button roll, hold;
+    private int score=0;
+    private int temp=0;
+    private int myScore=0;
+    private int rnd=0;
+    TextView p2txt;
 
-
+    TextView p1;
+    TextView round;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        myScore = intent.getIntExtra("score", 0);
+        score=intent.getIntExtra("p1",0);
+        rnd=intent.getIntExtra("round",0);
 
-//        die1 = (FrameLayout)findViewById(R.id.die1);
-//        die2 = (FrameLayout)findViewById(R.id.die2);
+        round=(TextView) findViewById(R.id.round);
+        round.setText("Round: "+rnd);
+        p2txt=(TextView) findViewById(R.id.p2);
+        p2txt.setText("P2: "+myScore);
 
+
+        p1=(TextView)findViewById(R.id.p1);
+        p1.setText("P1: "+score);
         roll = (Button) findViewById(R.id.button);
-
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rollDice();
+                p1.setText("P1: "+score);
+                if(score>=100) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Winner!");
+                    alertDialog.setMessage("You WIN!!!");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    Intent intent = new Intent(MainActivity.this,player2.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                    startActivity(intent);
+                                }
+                            });
 
+                    alertDialog.show();
+                }
             }
         });
-        hold = (Button) findViewById(R.id.hold);
 
+        hold = (Button)findViewById(R.id.hold);
         hold.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, player2.class);
-                intent.putExtra("score", 99);
+            public void onClick(View view) {
+                /*The commented out code here was the code we used
+                in class to send an integer to the next activty.
+                It was replaced by an alert dialog to be used to indicate
+                a winner (for demonstration purposes).
+                Use the alert dialog code in your program where appropriate*/
+                Intent intent = new Intent(MainActivity.this,player2.class);
+                intent.putExtra("round",rnd);
+                intent.putExtra("score", score);
+                intent.putExtra("p2",myScore);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 startActivity(intent);
-
             }
         });
 
-        die1 = (FrameLayout)findViewById(R.id.die1);
-        die2 = (FrameLayout)findViewById(R.id.die2);
-    }
 
-//get two random numbers, change the dice to have the appropriate image
-    public void rollDice()
-    {
-        int roll1 = 1 + (int)(6*Math.random());
-        int roll2 = 1 + (int)(6*Math.random());
-
-        setDie(roll1,die1);
-        setDie(roll2,die2);
-
-
-
+        die1 = (FrameLayout) findViewById(R.id.die1);
+        die2 = (FrameLayout) findViewById(R.id.die2);
 
     }
 
+    //get two random ints between 1 and 6 inclusive
+    public void rollDice() {
 
-// set appropriate image to frameview fo an int
-    public void setDie(int value,FrameLayout die)
-    {
-        Drawable pic = null;
-        switch (value)
+        int val1 = 1 + (int) (6 * Math.random());
+        int val2 = 1 + (int) (6 * Math.random());
+        if(val1==1 || val2==1)
         {
-            case 1:
-                pic= getResources().getDrawable(R.drawable.die1);
-                break;
-            case 2:
-                pic= getResources().getDrawable(R.drawable.die2);
-                break;
-            case 3:
-                pic= getResources().getDrawable(R.drawable.die3);
-                break;
-            case 4:
-                pic= getResources().getDrawable(R.drawable.die4);
-                break;
-            case 5:
-                pic= getResources().getDrawable(R.drawable.die5);
-                break;
-            case 6:
-                pic= getResources().getDrawable(R.drawable.die6);
-                break;
+
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Rolled a 1");
+                alertDialog.setMessage("You Rolled a 1, Points this round will not add to your score");
+                score-=temp;
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(MainActivity.this,player2.class);
+                                intent.putExtra("round",rnd);
+                                intent.putExtra("score", score);
+                                intent.putExtra("p2",myScore);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                startActivity(intent);
+                            }
+                        });
+                alertDialog.show();
 
         }
-        die.setBackground(pic);
+        else
+        {
+
+            setDie(val1, die1);
+            setDie(val2, die2);
+
+        }
+
     }
 
+    //set the appropriate picture for each die per int
+    public void setDie(int value, FrameLayout layout) {
+        Drawable pic = null;
+
+        switch (value) {
+            case 1:
+                pic = getResources().getDrawable(R.drawable.die1);
+
+
+                break;
+            case 2:
+                pic = getResources().getDrawable(R.drawable.die2);
+                score+=2;
+                temp+=2;
+                break;
+            case 3:
+                pic = getResources().getDrawable(R.drawable.die3);
+                score+=3;
+                temp+=3;
+                break;
+            case 4:
+                pic = getResources().getDrawable(R.drawable.die4);
+                score+=4;
+                temp+=4;
+                break;
+            case 5:
+                pic = getResources().getDrawable(R.drawable.die5);
+                score+=5;
+                temp+=5;
+                break;
+            case 6:
+                pic = getResources().getDrawable(R.drawable.die6);
+                score+=6;
+                temp+=6;
+                break;
+        }
+        layout.setBackground(pic);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- PairODice/app/src/main/java/com/example/leviandres/pairodice/player2.java
+++ PairODice/app/src/main/java/com/example/leviandres/pairodice/player2.java
@@ -1,111 +1,174 @@
 package com.example.leviandres.pairodice;
 
-        import android.app.AlertDialog;
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.graphics.drawable.Drawable;
-        import android.support.v7.app.ActionBarActivity;
-        import android.os.Bundle;
-        import android.view.FrameStats;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.FrameLayout;
-        import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class player2 extends ActionBarActivity {
-    private FrameLayout die1,die2;
-    private Button roll,hold;
-    private int score;
-
+    private FrameLayout die1, die2;
+    private Button roll, hold;
+    private int score=0;
+    private int myScore=0;
+    private int temp=0;
+    private int rnd=0;
+    TextView textElement;
+    TextView p1txt;
+    TextView round;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myplayer2);
 
-        Intent intent=getIntent();
-        score=intent.getIntExtra("score",0);
-        Toast.makeText(this,"The score is: "+score, Toast.LENGTH_LONG).show();
 
+        Intent intent = getIntent();
+            rnd=intent.getIntExtra("round",0);
+            round=(TextView) findViewById(R.id.round);
+            round.setText("Round: "+rnd);
 
-        roll = (Button)findViewById(R.id.button);
+        score = intent.getIntExtra("score", 0);
 
+            myScore=intent.getIntExtra("p2",0);
+            p1txt = (TextView) findViewById(R.id.p1);
+            p1txt.setText("P1: " + score);
+            textElement=(TextView)findViewById(R.id.p2);
+            textElement.setText("P2: "+myScore);
+
+        roll = (Button) findViewById(R.id.button);
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rollDice();
+                textElement.setText("P2: "+myScore);
+                if (myScore>=100)
+                {
+                    AlertDialog alertDialog=new AlertDialog.Builder(player2.this).create();
+                    alertDialog.setTitle("WINNER!");
+                    alertDialog.setMessage("You WIN!!!");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    Intent intent = new Intent(player2.this,MainActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                    startActivity(intent);
+                                }
+                            });
+                    alertDialog.show();
 
+
+                }
             }
         });
-        hold=(Button)findViewById(R.id.hold);
-        hold.setOnClickListener(new View.OnClickListener(){
+
+        hold = (Button)findViewById(R.id.hold);
+        hold.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                AlertDialog alertDialog=new AlertDialog.Builder(player2.this).create();
-                alertDialog.setTitle("You Won!");
-                alertDialog.setMessage("Yabadabadoo!");
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
+
+            public void onClick(View view) {
+                Intent intent = new Intent(player2.this,MainActivity.class);
+                intent.putExtra("round",rnd+1);
+                intent.putExtra("score", myScore);
+                intent.putExtra("p1", score);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(intent);
 
             }
-
         });
 
-        die1 = (FrameLayout)findViewById(R.id.die1);
-        die2 = (FrameLayout)findViewById(R.id.die2);
-    }
-
-    //get two random numbers, change the dice to have the appropriate image
-    public void rollDice()
-    {
-        int roll1 = 1 + (int)(6*Math.random());
-        int roll2 = 1 + (int)(6*Math.random());
-
-        setDie(roll1,die1);
-        setDie(roll2,die2);
-
-
-
+        die1 = (FrameLayout) findViewById(R.id.die1);
+        die2 = (FrameLayout) findViewById(R.id.die2);
 
     }
 
-
-    // set appropriate image to frameview fo an int
-    public void setDie(int value,FrameLayout die)
-    {
-        Drawable pic = null;
-        switch (value)
+    //get two random ints between 1 and 6 inclusive
+    public void rollDice() {
+        int val1 = 1 + (int) (6 * Math.random());
+        int val2 = 1 + (int) (6 * Math.random());
+        if(val1==1 || val2==1)
         {
-            case 1:
-                pic= getResources().getDrawable(R.drawable.die1);
-                break;
-            case 2:
-                pic= getResources().getDrawable(R.drawable.die2);
-                break;
-            case 3:
-                pic= getResources().getDrawable(R.drawable.die3);
-                break;
-            case 4:
-                pic= getResources().getDrawable(R.drawable.die4);
-                break;
-            case 5:
-                pic= getResources().getDrawable(R.drawable.die5);
-                break;
-            case 6:
-                pic= getResources().getDrawable(R.drawable.die6);
-                break;
+            AlertDialog alertDialog=new AlertDialog.Builder(player2.this).create();
+            alertDialog.setTitle("Rolled a 1");
+            alertDialog.setMessage("You Rolled a 1, Points this round will not add to your score");
+            myScore-=temp;
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(player2.this,MainActivity.class);
+                            intent.putExtra("round",rnd+1);
+                            intent.putExtra("score", myScore);
+                            intent.putExtra("p1", score);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            startActivity(intent);
+                        }
+                    });
+            alertDialog.show();
+
 
         }
-        die.setBackground(pic);
+        else
+        {
+            setDie(val1, die1);
+            setDie(val2, die2);
+        }
+
     }
 
+    //set the appropriate picture for each die per int
+    public void setDie(int value, FrameLayout layout) {
+        Drawable pic = null;
+
+        switch (value) {
+            case 1:
+                pic = getResources().getDrawable(R.drawable.die1);
+
+
+                break;
+            case 2:
+                pic = getResources().getDrawable(R.drawable.die2);
+                myScore+=2;
+                temp+=2;
+                break;
+            case 3:
+                pic = getResources().getDrawable(R.drawable.die3);
+                myScore+=3;
+                temp+=3;
+
+                break;
+            case 4:
+                pic = getResources().getDrawable(R.drawable.die4);
+                myScore+=4;
+                temp+=4;
+
+                break;
+            case 5:
+                pic = getResources().getDrawable(R.drawable.die5);
+                myScore+=5;
+                temp+=5;
+
+                break;
+            case 6:
+                pic = getResources().getDrawable(R.drawable.die6);
+                myScore+=6;
+                temp+=6;
+
+                break;
+        }
+        layout.setBackground(pic);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
